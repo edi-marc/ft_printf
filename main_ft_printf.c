0372200cb70240c70dd236f4326125ab86b419a9
edi-marc <edi-marc@student.42roma.it>
2021-02-21 18:32:31 +0100

    added some usefull Resources

--- main_ft_printf.c
+++ main_ft_printf.c
@@ -6,19 +6,36 @@
 /*   By: edi-marc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/19 17:53:54 by edi-marc          #+#    #+#             */
-/*   Updated: 2021/02/19 18:19:37 by edi-marc         ###   ########.fr       */
+/*   Updated: 2021/02/21 18:31:23 by edi-marc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include <stdio.h>
 
+void	red(void)
+{
+	printf("\033[0;31m");
+}
+
+void	green(void)
+{
+	printf("\033[0;32m");
+}
+
 void	check_printf(int ft, int origin)
 {
+	printf("\n");
 	if (origin == ft)
+	{
+		green();
 		printf("result = [OK]\n");
+	}
 	else
+	{
+		red();
 		printf("result = [KO] -- ft=%d - original=%d\n", ft, origin);
+	}
 }
 
 int		main(void)
@@ -26,9 +43,9 @@ int		main(void)
 	int	ft;
 	int	origin;
 
-	origin = printf("ciao%%%%%%%%");
+	origin = printf("ciao");
 	printf("\n");
-	ft = 0;
+	ft = 4;
 	check_printf(ft, origin);
 	return (0);
 }
